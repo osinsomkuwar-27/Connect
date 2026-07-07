6b669a0666e09e6d649f092e4a1e858ee3bdfb7c
Osin Somkuwar <osinsomkuwar26@gmail.com>
2026-07-07 04:33:43 +0000

    feat: update authentication routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ import {Server} from "socket.io";
 import mongoose from "mongoose";
 import { connectTosocket } from "./controllers/socketManager.js";
 import cors from "cors";
-
+import userRoutes from "./routes/users.routes.js";
 dotenv.config();
 const app = express();
 const server = createServer(app);
@@ -17,6 +17,8 @@ app.set("port", (process.env.PORT || 8000))
 app.use(cors());
 app.use(express.json({limit:"40kb"}));
 app.use(express.urlencoded({limit:"40kb", extended:true}));
+
+app.use("/api/v1/users", userRoutes);
 
 const start = async () =>{
     const connectionDb = await mongoose.connect(process.env.MONGODB_URI);
--- backend/src/models/meeting.model.js
+++ backend/src/models/meeting.model.js
@@ -1,9 +1,9 @@
 import mongoose, {Schema} from "mongoose";
 
 const meetingSchema = new Schema({
-    user_id = {type:String},
-    meetingCode = {type:String, required : true},
-    date = {type:Date, default: Date.now, required : true},
+    user_id: {type:String},
+    meetingCode: {type:String, required : true},
+    date: {type:Date, default: Date.now, required : true},
 });
 
 const user = mongoose.model("Meeting", meetingScheme);
