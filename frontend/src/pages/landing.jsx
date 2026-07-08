04c4eb8bdfcf653146edb21eefac447b4ff144dc
Osin Somkuwar <osinsomkuwar26@gmail.com>
2026-07-08 17:25:59 +0000

    feat: create landing page

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
+import "../App.css"
+import { Link } from 'react-router-dom';
 
 export default function LandingPage() {
     return (
-        <div>LandingPage</div>
+        <div className='landingPageContainer'>
+            <nav>
+                <div className='navHeader'>
+                    <h2>Connect</h2>
+                </div>
+                <div className='navlist'>
+                    <p>Join as Guest</p>
+                    <p>Register</p>
+                    <div role='button'>
+                        <p>Login</p>
+                    </div>
+                </div>
+            </nav>
+
+            <div className="landingMainContainer">
+                <div>
+                    <h1><span style={{color: "#FF9839"}}>Connect</span> with your loved ones</h1>
+                    <p>Cover the distance by Connect</p>
+                    <div role='button'>
+                        <Link to={"/home"}>Get Started</Link>
+                    </div>
+                </div>
+                <div>
+                    <img src="/mobile.png" alt=""/>
+                </div>
+            </div>
+
+        </div>
     )
 }
