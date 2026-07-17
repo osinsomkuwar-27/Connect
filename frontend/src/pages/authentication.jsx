e98299fe1164e7d0b0b3c7d3d85f77780f9b6c87
osinsomkuwar-27 <osinsomkuwar26@gmail.com>
2026-07-17 18:12:05 +0530

    feat: build webrtc connections, screen share, chat messages feature

--- frontend/src/pages/authentication.jsx
+++ frontend/src/pages/authentication.jsx
@@ -66,7 +66,8 @@ export default function Authentication() {
                     sm={4}
                     md={7}
                     sx={{
-                        backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
+                        backgroundImage:
+  "url('https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=1200&q=80')",
                         backgroundRepeat: 'no-repeat',
                         backgroundColor: (t) =>
                             t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
